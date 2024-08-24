063eb3c39e566434ff57f553f40476fb5344b781
Tomoya Kawanishi <tomoya.kaw@gmail.com>
2024-08-24 16:00:15 +0900

    add window.electron.callGuest function type definition

--- src/type.d.ts
+++ src/type.d.ts
@@ -2,5 +2,8 @@ interface Window {
   fs?: {
     readFile: (path: string) => Promise<string>;
     toFileUrl: (relativePath: string) => Promise<string>;
+  },
+  electron: {
+    callGuest: (guestWebContentsId: number, funcName: string, args?: [any]) => Promise<any>;
   }
 }
