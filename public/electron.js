ddeb4250c91f4d21b1e6894e7b4e01ae667f1760
Tomoya Kawanishi <tomoya.kaw@gmail.com>
2024-08-24 16:04:38 +0900

    add IPC for nodejs process

--- public/electron.js
+++ public/electron.js
@@ -1,7 +1,8 @@
-const { app, BrowserWindow, ipcMain } = require('electron');
+const { app, BrowserWindow, ipcMain, webContents } = require('electron');
 const fs = require('fs');
 const path = require('path');
 const { promisify } = require('util');
+const { v4: uuidv4 } = require('uuid');
 
 function createWindow() {
   const win = new BrowserWindow({
@@ -50,8 +51,6 @@ ipcMain.handle('read-file', (event, filePath) => {
 });
 
 ipcMain.handle('to-file-url', (event, relativePath) => {
-  console.log("hoge");
-  console.log(relativePath);
   const absolutePath = path.resolve(__dirname, "..", relativePath);
   const fileUrl = `file://${absolutePath.replace(/\\/g, '/')}`
   return fileUrl;
@@ -60,3 +59,58 @@ ipcMain.handle('to-file-url', (event, relativePath) => {
 ipcMain.on('message-from-webview', (event, message) => {
   console.log('Received message from webview:', message);
 });
+
+const functions = {
+  'hello': () => {
+    return {
+      status: 'success',
+      message: 'Hello from Nodejs'
+    };
+  }
+};
+
+ipcMain.on('nodejs-request', async (event, request) => {
+  console.log(request);
+
+  const { function: functionName, args, responseChannel } = request;
+  let response;
+
+  try {
+    const func = functions[functionName];
+    if (func) {
+      const result = func(...args);
+      response = { status: 'success', message: result };
+    } else {
+      response ={ status: 'error', message: 'Unknown function' };
+    }
+  } catch (error) {
+    response = { status: 'error', message: error.message };
+  }
+
+  event.sender.send(responseChannel, response);
+});
+
+ipcMain.handle('guest-request', (event, request) => {
+  return new Promise((resolve, reject) => {
+    const { guestWebContentsId, function: functionName, args } = request;
+    const responseChannel = `guest-response-${uuidv4()}`;
+
+    const guestWebContents = webContents.fromId(guestWebContentsId);
+    const message = {
+      function: functionName, args, responseChannel
+    };
+    guestWebContents.send('guest-request', message);
+
+    ipcMain.once(responseChannel, (event, response) => {
+      console.log(response);
+      if (response.status === 'success') {
+        resolve(response.message);
+      } else {
+        reject(new Error(response.message || 'Failed to execute function in guest page'));
+      }
+    });
+  });
+});
+
+
+
